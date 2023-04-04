13ca71deb40cd938bb6b640d8440a05978d79ab8
nicolycunha <nicolyoliveiracunha.2001@gmail.com>
2023-04-04 18:28:48 -0300

    utilização de propriedades nos componentes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video
+          likes={100}
+          messages={200}
+          shares={300}
+          name="Nicoly"
+          description="O gato goleiro"
+          music="musica épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+        <Video
+          likes={444}
+          messages={455}
+          shares={553}
+          name="Pedro"
+          description="O gato fofinho"
+          music="musica qualquer"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
       </div>
     </div>
   )
